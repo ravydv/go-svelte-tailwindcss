df93ff39148dac91aa8c9d81f0a3f9b991d35d30
Ravi Yadav <yadavravi@microsoft.com>
2022-03-11 21:23:38 +0530

    live reload test

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 
 <main class="text-center">
 	<h1 class="text-orange-600">Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Visit the <a href="https://svelte.dev/tutorial" class="text-blue-600 underline">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style >
